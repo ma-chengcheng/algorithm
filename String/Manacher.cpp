bbf162a4146e3defc2087279d3a62d23cfa17c79
machengcheng <616254086@qq.com>
2017-10-28 20:34:45 +0800

    Manacher回文算法

--- String/Manacher.cpp
+++ String/Manacher.cpp
@@ -9,63 +9,52 @@ using namespace std;
 * 作用：求出一个字符串以任意字符串为中心的最长回文子串的长度
 * 实现步骤：
 * 1、预处理，将字符串每两个字符串之间加上一个特殊符号’＃‘’
-* 2、计算字符的前缀的Hash值
-* 3、得到[i, j)区间内的子串的hash值
+* 2、计算每一个以s[i]为中心的回文
 */
 
 // 字符串最长长度
-const int N = 1000 + 5;
-// 处理后的字符串
-char manacher[N];
-// 用以保存以manacher[i]为中心的最长回文串长度的一半
-int a[N];
+const int N = 110000 + 5;
+char s[N];
+char m[N + N];
+int a[N + N];
+int len;
 
-// 预处理
-void init(string &s) {
-  manacher[0] = s[0];
-  for (int i = 1; i < s.size(); i++) {
-    manacher[2 * i - 1] = '#';
-    manacher[2 * i] = s[i];
+void init(){
+  len = strlen(s);
+  m[0] = '#';
+  for (int i = 0; i < len; i++) {
+    m[i * 2 + 1] = s[i];
+    m[i * 2 + 2] = '#';
   }
+  len = 2 * len + 1;
 }
 
-int Manacher(int len) {
+int manacher(){
+  memset(a, 0, sizeof(a));
   a[0] = 0;
-  int i = 0, j;
-  int left , right;
-  int ans = 0;
+  int i = 0, j, ans = 0;
   while (i < len) {
-    // 当在以manacher[i]没有超过越过左边的边界，并且manacher[i + a[i]]的左右两边的字符相等时
-    while (i - a[i] > 0 && manacher[(i + a[i]) + 1] == manacher[(i - a[i]) - 1 ]) {
-      a[i] += 1;
+    while (i - a[i] > 0 && i + a[i] <= len && m[i + a[i] + 1] == m[i - a[i] - 1]) {
+      a[i]++;
     }
-    // 更新最小的回文长度
-    ans = min(ans, a[i]);
+    ans = max(ans, a[i]);
     j = i + 1;
-    // left和right为manacher[i]左右下标
-    left = i - (j - i);
-    right = j;
-    // 如何为第一、二种情况游标向右，右边在以manacher[i]为中心的回文串内并且以manacher[left]的最左边与manacher[i]不能相等
-    while (right <= i + a[i] && i - a[i] != left - a[left]) {
-      a[j] = min(a[left], a[i] - (j - i));
-      j +=1;
-      left = i - (j - i);      
-      right = j;
+    while (j <= i + a[i] && i - a[i] != i - (j - i) - a[i - (j - i)]) {
+      a[j] = min(a[i - (j - i)], a[i] - (j - i));
+      j++;
     }
-    // 如果为第三种情况，manacher[left]的最左端恰好等于manacher[i]的最左边时，
-    // 不能判断a[right]为多少，只知道会比它大
-    a[j] = max(a[i] - (j - i), 0);
+    a[j] = max(a[i] - (j - i) , 0);
     i = j;
   }
   return ans;
 }
 
 int main() {
-  string s;
-  while (cin >> s) {
-    init(s);
-    int ans = Manacher(2 * s.size() -1);
-    cout << ans << endl;
-    cout << '\n';
+
+  while (scanf("%s", s) != EOF) {
+    init();
+    int ans = manacher();
+    printf("%d\n", ans);
   }
+  return 0;
 }
